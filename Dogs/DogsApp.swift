f9967c0ae36afce9259c81c1332faddb08dc1b8a
MaelysCallens <100351753+MaelysCallens@users.noreply.github.com>
2023-12-29 13:39:36 +0100

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Maëlys Callens on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
